5a5dd7c7019c31bd2feb4d669a5e248dbaa68f28
Parfan <gabrielparfan@hotmail.com>
2021-11-01 03:15:46 -0300

    Salvar exemplo acessado na sessionStorage

--- Codigo/src/js/exemplos.js
+++ Codigo/src/js/exemplos.js
@@ -15,35 +15,43 @@ function getExamples() {
     return JSON.parse(sessionStorage.getItem("allExamples"))["exemplos"];
 }
 
+function accessedExample(example) {
+    sessionStorage.setItem("accessedExample", JSON.stringify(example));
+}
+
 function addCardExamples(examples) {
     let cardAmount = $(".container > .row").children().length;
     let exampleAmount = examples.length;
 
+    // Gera 4 cards no container
     for (let i = cardAmount; i < cardAmount + 4; i++) {
+        // Para a execucao se nao houver mais cards para carregar
         if (i >= exampleAmount) {
             break;
         }
         let example = examples[i];
-        let title = example["titulo"];
-        let image = example["imagem"];
-        let description = example["descricao"];
-        $(".container > .row").append($(`<div class="col-6"></div>`).append(
+        $(".container > .row").append($("<div id='aaa' class='col-6'></div>").append(
             $("<img />", {
-                src: image,
-                alt: title
+                src: example["imagem"],
+                alt: example["titulo"]
             }),
-            $(`<h2>${title}</h2>`),
-            $(`<p>${description}</p>`),
+            $(`<h2>${example["titulo"]}</h2>`),
+            $(`<p>${example["descricao"]}</p>`),
             $("<a></a>", {
+                id: example["id"],
                 text: "Mais sobre",
-                href: example["url"],
-                target: "_blank"
+                href: "./exemplo.html"
             })
         ));
     }
+    // Se nao houver mais cards a serem carregados o botao desativa
     if (cardAmount + 4 >= exampleAmount) {
         $("#load-examples").hide();
     }
+
+    $(".container a").click(function() {
+        sessionStorage.setItem("example", JSON.stringify(examples[parseInt(this.id)]));
+    });
 }
 
 $("#load-examples").click(() => {
